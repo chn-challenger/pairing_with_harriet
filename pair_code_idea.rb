6f7044f121b88eb62fd6fb999726fac4cdaaa541
Harriet Craven <harrietcraven@Harriets-MacBook-Pro.local>
2015-08-21 16:12:20 +0100

    Final edit

--- pair_code_idea.rb
+++ pair_code_idea.rb
@@ -1,21 +1,55 @@
-def random_pair1 array
-length = array.length
-array.shuffle!
-if length.odd?
-	p"You need to have an even number of people to pair"
-	p 
-	p"So I have removed somebody randomly"
-	p
-	p "The unlucky person is"
-	loner = array.pop
-	p loner.to_s
-end
-	puts "Here are the random pairings"
+# def random_pair1 array
+# length = array.length
+# array.shuffle!
+# if length.odd?
+# 	p"You need to have an even number of people to pair"
+# 	p 
+# 	p"So I have removed somebody randomly"
+# 	p
+# 	p "The unlucky person is"
+# 	loner = array.pop
+# 	p loner.to_s
+# end
+# 	puts "Here are the random pairings"
 	
-	array.each_slice(2) do |b|
-    p b[0] + ' is paired with ' + b[1]
-end
-end
+# 	array.each_slice(2) do |b|
+#     p b[0] + ' is paired with ' + b[1]
+# end
+# end
+
+
+
+
+# def new_pairing(names_array,*paired_names)
+# 	original_people_array = names_array
+# 	original_paired_names = paired_names
+
+
+# 	new_pair = random_pair(names_array)
+# 	new_pair.each do |pair| 
+# 		pair.sort!
+# 	end
+# 	#p new_pair
+# 	existing_pairs = paired_names.each do |pair| 
+# 		pair.sort!
+# 	end
+# 	#p existing_pairs
+
+# 	p names_array
+
+# 	for i in 0...new_pair.length do
+# 		for j in 0...existing_pairs.length do
+# 			if existing_pairs[j] == new_pair[i]
+# 				return new_pairing(original_people_array,original_paired_names)
+# 			end
+# 		end
+# 	end
+
+
+
+# 	return new_pair
+
+# end
 
 
 def random_pair array
@@ -34,19 +68,19 @@ def random_pair array
 end
 
 
-
-
 people = ['Harriet','Lucy','Joe','Tim','Sarah','Usman','Matt','Adrian','Yana']
-pairing1 = [["Yana", "Joe"], ["Usman", "Matt"], ["Adrian", "Sarah"], ["Lucy", "Tim"]]
-pairing2 = [["Sarah", "Yana"], ["Lucy", "Matt"], ["Tim", "Usman"], ["Adrian", "Harriet"]]
+pairing1 = [["Yana", "Joe"], ["Usman", "Matt"], ["Adrian", "Sarah"], ["Lucy", "Tim"], ["Sarah", "Yana"], ["Lucy", "Matt"], ["Tim", "Usman"], ["Adrian", "Harriet"]]
 
 
-
-
-def new_pairing(names_array,*paired_names)
-	original_people_array = names_array
-	original_paired_names = paired_names
-
+def new_pairing(names_array,paired_names)
+	original_people_array = []
+	names_array.each do |name|
+			original_people_array << name
+	end
+	original_paired_names = []
+	paired_names.each do |pairs|
+		original_paired_names << pairs
+	end
 
 	new_pair = random_pair(names_array)
 	new_pair.each do |pair| 
@@ -58,46 +92,12 @@ def new_pairing(names_array,*paired_names)
 	end
 	#p existing_pairs
 
-	p names_array
+	#p names_array
 
 	for i in 0...new_pair.length do
 		for j in 0...existing_pairs.length do
 			if existing_pairs[j] == new_pair[i]
-				return new_pairing(original_people_array,original_paired_names)
-			end
-		end
-	end
-
-
-
-	return new_pair
-
-end
-
-
-
-
-
-def new_pairing1(names_array,*paired_names)
-	original_people_array = names_array
-	original_paired_names = paired_names
-
-
-	new_pair = random_pair(names_array)
-	new_pair.each do |pair| 
-		pair.sort!
-	end
-	#p new_pair
-	existing_pairs = paired_names.flatten(1).each do |pair| 
-		pair.sort!
-	end
-	#p existing_pairs
-
-	p names_array
-
-	for i in 0...new_pair.length do
-		for j in 0...existing_pairs.length do
-			if existing_pairs[j] == new_pair[i]
+				puts "Recursion is going on.."
 				return new_pairing(original_people_array,original_paired_names)
 			end
 		end
@@ -114,7 +114,7 @@ def new_pairing1(names_array,*paired_names)
 	#for best practice: try to make the script efficient (fast) as possible.
 end
 
-p new_pairing(people, pairing1, pairing2)
+p new_pairing(people, pairing1)
 
 #p new_pairing(['A','B','C','D','E','F'],[['A','C'],['B','D']],[[],[],[]])
 
